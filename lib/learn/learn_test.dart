9a53a138c786cbe909cac91249fec67157cfa035
lupanfeng <panfeng.lu@gmail.com>
2020-01-19 20:02:51 +0800

    commit

--- lib/learn/learn_test.dart
+++ lib/learn/learn_test.dart
@@ -2,8 +2,11 @@
 void main() {
   selectSort();
   countdown(4);
+  print(recursiveSum([1,2,3,4,5]));
+  print(recursiveListNum([1,2,3,4,5]));
 }
 
+/// 选择排序
 selectSort() {
   var list = [1,4,5,6,2,8,9,0];
 
@@ -29,4 +32,19 @@ countdown(int i) {
   if (i < 0) return;
   print(i);
   countdown(i-1);
+}
+
+recursiveSum(List<int> list) {
+  if(list.length == 0) {
+    return 0;
+  }
+  return list[0] + recursiveSum(list.sublist(1));
+}
+
+recursiveListNum(List<int> list) {
+  if (list.isEmpty) {
+    return 0;
+  }
+  return 1 + recursiveListNum(list.sublist(1));
+
 }
